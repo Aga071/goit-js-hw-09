5a647daca649efc4fd38a3c58fcabf6931ad49e8
Agata Warzyszyńska <agata.wlazlak@gmail.com>
2023-08-26 20:07:35 +0200

    js-hw-9-secend-version-v2

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -6,19 +6,19 @@ function createPromise(position, delay) {
   setTimeout(() => {
     if (shouldResolve) {
       return Promise.resolve(
-        `Fulfilled promise ${position} in ${Number(firstDelay.value) + delay}ms`
+        `Fulfilled promise ${position} in ${delay}ms`
       ).then(text => {
         Notiflix.Notify.success(text);
         console.log(text);
       });
       // Fulfill
     } else {
-      return Promise.reject(
-        `Rejected promise ${position} in ${Number(firstDelay.value) + delay}ms`
-      ).catch(error => {
-        Notiflix.Notify.failure(error);
-        console.log(error);
-      });
+      return Promise.reject(`Rejected promise ${position} in ${delay}ms`).catch(
+        error => {
+          Notiflix.Notify.failure(error);
+          console.log(error);
+        }
+      );
       // Reject
     }
   }, delay);
@@ -26,10 +26,7 @@ function createPromise(position, delay) {
 
 Btn.addEventListener('click', e => {
   e.preventDefault();
-
-  setTimeout(() => {
-    for (let i = 1; i <= amount.value; i++) {
-      createPromise(i, (i - 1) * delayStep.value);
-    }
-  }, firstDelay.value);
+  for (let i = 1; i <= amount.value; i++) {
+    createPromise(i, Number(firstDelay.value) + (i - 1) * delayStep.value);
+  }
 });
